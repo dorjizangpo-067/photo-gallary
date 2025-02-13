675ee5ecef42004476719e0509740328b1179585
dorjizangpo-067 <dorjizangpo067@gmail.com>
2025-02-13 17:26:00 +0600

    collect static file

--- base/settings.py
+++ base/settings.py
@@ -132,6 +132,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'app' / 'static',
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
